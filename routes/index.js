6730746924cf44b32bca92d7f90d9259ccdfeb9f
Anthony Delgado <me@anthonydelgado.me>
2016-10-29 11:25:40 -0400

    add routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
-
+var waitlist = require('')
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/create', function(req, res, next) {
+  res.render('index', { title: 'Make Reservations' });
+});
+
+router.get('/view', function(req, res, next) {
+  res.render('index', { title: 'View Reservations' });
+});
+
+router.get('/api/waitlist', function(req, res, next) {
+  res.json('index', { title: 'View Reservations' });
+});
+
+router.get('/api/tables', function(req, res, next) {
+  res.json('index', { title: 'View Reservations' });
 });
 
 module.exports = router;
